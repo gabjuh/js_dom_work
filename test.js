2a865fd565ff3403eb3d97a71e9a59fa30461e56
Gabor Juhasz <adgbe@yahoo.de>
2021-09-13 11:50:18 +0200

    saved files

---
+++ test.js
@@ -0,0 +1,6 @@
+const changeH1 = text => document.querySelectorAll('h1').forEach(h => h.innerHTML = text)
+
+const changeP = par => document.querySelectorAll('p').forEach(p => p.innerHTML = par)
+
+const changeMenu = menu => document.querySelectorAll('li').forEach(l => l.innerHTML = menu)
+q
---
+++ uebung06.js
@@ -0,0 +1,17 @@
+// 1
+$$('img[src$=jpg]')
+
+// 2
+$$('form input[type=button]')
+
+// 3
+$$('.model[data-model*=V7]')
+
+// 4
+$$('img:not(.float_left)')
+
+// 5
+$$('li:nth-child(2)')
+
+// 6
+$('h2 + ul')
