365ad9f8d9ce6ecbe5aad51208c6c6143a0b74c7
Gabor Juhasz <adgbe@yahoo.de>
2021-09-22 14:42:41 +0200

    save

--- arb_mat/05/exercises/click_me.js
+++ arb_mat/05/exercises/click_me.js
@@ -1,12 +1,43 @@
 'use strict'
 
 {
+
+    const init = () => $('#click_me').addEventListener('click', handleButtonClick)
+
+    const handleButtonClick = event =>
+        changeButtonTextTo(event.target, event.altKey 
+            ? 'Cool, you found an easteregg!'
+            : 'Hey I like it when you click me!'
+        )
+    
+    const changeButtonTextTo = (button, text) => (button.innerHTML = text)
+
     const $ = q => document.querySelector(q)
-    const $$ = q => Array.from(document.querySelectorAll(q))
+    // const $$ = q => Array.from(document.querySelectorAll(q))
     const $on = (el, ev, fn) => {
         Array.isArray(el)
             ? el.forEach(o => $on(o, ev, fn))
             : el.addEventListener(ev, fn)
         return el
     }
+
+    init()
+
+
+
+
+
+
+
+
+
+    // $on($('#click_me'), 'click', () => alert('Hey I Like it when you click me'))
+    
+    // $on($('#click_me'), 'click', e => e.target.innerHTML = 'Bla')
+
+    // $on(
+    //     $on($('click_me'), 'click', () => console.log('bla')),
+    //     'altKey',
+    //     e => e.target.innerHTML = 'Easern Egg! :-)'
+    // )
 }
